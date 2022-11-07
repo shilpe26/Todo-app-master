93b2d2a826aeb2a9bcf951257b42d69e6714d2b8
shilpecsaxena9098@gmail.com <shilpecsaxena9098@gmail.com>
2022-11-07 16:57:01 +0530

    add: error handling of form validation

--- todo/src/todoComponents/AddTodo.jsx
+++ todo/src/todoComponents/AddTodo.jsx
@@ -1,37 +1,61 @@
 import React, { useState } from "react";
 import { addTodo } from "./Api";
 
+const initTodoState = {
+	title:"",
+	description:"",
+	status:false
+}
 
 function AddTodo({callbackAfterAddTodoSuccess}) {
 	const [todo, setTodo] = useState({
-		title:"",
-		description:"",
-		status:false
+		...initTodoState
 	});
+	const [errors,setErrors] = useState({});
+
+	const addTodoFormRules = (data) => {
+		const error = {};
+		const {title,description} = data;
+		if(!title){
+			error.title = "Title is required";
+		}
+		if(!description){
+			error.description = "Description is required"
+		}if(title.length <3){
+			error.title = "Title should be 3 characters long!"
+		}if(description.length < 10){
+			error.description = "Discription should be 10 characters long!."
+		}
+		return error
+	}
+
 
 	const handleTodoFormSubmit = (e) => {
-		e.preventDefault()
-		
+		e.preventDefault();
+		const error = addTodoFormRules(todo);
+		if(Object.keys(error).length>0 ||Object.keys(errors).length>0  ){
+			console.log(error)
+			setErrors({...error});
+			return;
+		}
 		const {title,description,status} = todo;
 		const addTodoParams = {name:title,description,status}
-		//1) TODO : Check Form Validation
-		//2) TODO : Only proceed if form is validated
-		if(title !== "" && description !== ""){
 			addTodo(addTodoParams)	
 			.then((data)=> { 
 				callbackAfterAddTodoSuccess()
 				console.log(data)
 			})
 			.catch((error) => console.log("Issue while adding todo",error))
-		}else{
-			console.log("Fill the fields")
-		}
-		setTodo("");
+		
+		setTodo({...initTodoState});
 	}
 
 	const handleTodoFormChange = (e) => {
 		const fieldName = e.target.name;
 		const fieldValue = e.target.value;
+		const error = {...errors};
+		delete error[fieldName];
+		setErrors(error)
 		setTodo({...todo,[fieldName]:fieldValue})
 	}
 
@@ -44,10 +68,11 @@ function AddTodo({callbackAfterAddTodoSuccess}) {
 				value={todo.title}
 				onChange={handleTodoFormChange}
 			/>
+			{errors["title"] && <span style={{color:"red"}}>{errors["title"]}</span>}
 			<div className="textarea-field">
 			<textarea value={todo.description} name="description" onChange={handleTodoFormChange} placeholder="write your complete todo">
-			
 			</textarea>
+			{errors["description"] && <span style={{color:"red"}}>{errors["description"]}</span>}
 			</div>
 
 			<button className="add-btn">
