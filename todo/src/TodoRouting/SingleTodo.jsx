c2ac61f3b5970d089cbdae7a28bb4b5fee8c2ec6
shilpecsaxena9098@gmail.com <shilpecsaxena9098@gmail.com>
2022-11-07 17:32:23 +0530

    doc: no change

--- todo/src/TodoRouting/SingleTodo.jsx
+++ todo/src/TodoRouting/SingleTodo.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoItems from '../todoComponents/TodoItem'
+import axios from "axios"
+
 
 function SingleTodo({id, name}) {
+  // const [showData, setShowData] = useState("")
+  // const getTodos = () => {
+  //   return axios.get(`http://localhost:8080/todo/${id}`);
+  // }
   return (
     <div>Your Single Todo is here
+
+ 
         <TodoItems id={id} name={name} />
 
     </div>
