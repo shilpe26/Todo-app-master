6c2674552c00237e54faa281c1f52e93f0c73cea
shilpecsaxena9098@gmail.com <shilpecsaxena9098@gmail.com>
2022-11-07 16:59:44 +0530

    fix: styling

--- todo/src/todoComponents/AddTodo.jsx
+++ todo/src/todoComponents/AddTodo.jsx
@@ -21,7 +21,7 @@ function AddTodo({callbackAfterAddTodoSuccess}) {
 		}
 		if(!description){
 			error.description = "Description is required"
-		}if(title.length <3){
+		}if(title.length < 3){
 			error.title = "Title should be 3 characters long!"
 		}if(description.length < 10){
 			error.description = "Discription should be 10 characters long!."
