2a10675211b7d8ea8364ea44c6f8e1d6baefc428
shilpecsaxena9098@gmail.com <shilpecsaxena9098@gmail.com>
2022-11-08 08:40:19 +0530

    fix: Single todo page

--- todo/src/TodoRouting/SingleTodo.jsx
+++ todo/src/TodoRouting/SingleTodo.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
 import TodoItems from '../todoComponents/TodoItem'
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import axios from "axios"
 
-
-function SingleTodo({id, name}) {
-  // const [showData, setShowData] = useState("")
-  // const getTodos = () => {
-  //   return axios.get(`http://localhost:8080/todo/${id}`);
-  // }
+function SingleTodo() {
+  let {id} = useParams();
+  const [showData, setShowData] = useState("")
+ 
+  
+  useEffect(() => {
+    async function getTodoById(id){
+      const {data} = await axios.get(`http://localhost:8080/todo/${id}`)
+      setShowData(data)
+  
+    }
+    getTodoById(id)
+  },[id])
   return (
     <div>Your Single Todo is here
 
  
-        <TodoItems id={id} name={name} />
+        <TodoItems id={showData.id} name={showData.name} />
 
     </div>
   )
