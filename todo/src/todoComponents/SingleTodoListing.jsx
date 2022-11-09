0422bf94b2da09f4e8e70d5c4320d0850afd9c2a
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-11-09 22:34:08 +0530

    Update SingleTodoListing.jsx

--- todo/src/todoComponents/SingleTodoListing.jsx
+++ todo/src/todoComponents/SingleTodoListing.jsx
@@ -1,53 +1,60 @@
 import React, { useCallback, useEffect } from "react";
 import { AiFillDelete } from "react-icons/ai";
-import { MdPendingActions, MdDoneOutline } from "react-icons/md";
+import { MdDoneOutline, MdPendingActions } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-  deleteTodo,
-  getTodoByIdRenderCall,
-  toggleTodo,
+	deleteTodo,
+	getTodoByIdRenderCall,
+	toggleTodo,
 } from "../actions/todoActions";
 
-function SingleTodoListing({ id, name, description, status }) {
-  const dispatch = useDispatch();
-  const getTodoById = useSelector((state) => state.getTodoById);
+function SingleTodoListing({ id }) {
+	const dispatch = useDispatch();
+	const getTodoById = useSelector((state) => state.getTodoById);
+	const { todo, loading, error, success } = getTodoById;
 
-  const getTodoByIdData = useCallback(() => {
-    dispatch(getTodoByIdRenderCall(id));
-  }, [dispatch, id]);
+	const getTodoByIdData = useCallback(() => {
+		dispatch(getTodoByIdRenderCall(id));
+	}, [dispatch, id]);
 
-  useEffect(() => {
-    getTodoByIdData();
-  }, [getTodoByIdData]);
+	useEffect(() => {
+		getTodoByIdData();
+	}, []);
+	const handleDelete = (id) => {
+		dispatch(deleteTodo(id));
+	};
 
-  const handleDelete = (id) => {
-    dispatch(deleteTodo(id));
-  };
-
-  const handleToggle = (id, newStatus) => {
-    const toggleTodoParams = { id, newStatus };
-    dispatch(toggleTodo(toggleTodoParams, () => getTodoByIdData()));
-  };
-
-  return (
-    <div>
-      <h2 className="todo-id">
-        {id}
-        <Link to={`/todo/${id}`}>
-          <div className="todo-name">
-            {name}
-            <div className="todo-desc">{description}</div>
-          </div>
-        </Link>
-      </h2>
-      <button onClick={() => handleToggle(id, !status)}>
-        {status ? <MdDoneOutline /> : <MdPendingActions />}
-      </button>
-      <button onClick={() => handleDelete(id)}>
-        <AiFillDelete />
-      </button>
-    </div>
-  );
+	const handleToggle = (id, newStatus) => {
+		const toggleTodoParams = { id, newStatus };
+		dispatch(toggleTodo(toggleTodoParams, () => getTodoByIdData()));
+	};
+	return (
+		<div>
+			{loading ? (
+				<h1>App is Loading</h1>
+			) : error ? (
+				<h1>{error}</h1>
+			) : success ? (
+				// Single todo lsiting page
+				<div className="single-page-todo">
+					Your Complete Todo is here
+					<div>{todo?.name}</div>
+					<div>{todo?.description}</div>
+					<div>
+						<button onClick={() => handleToggle(id, !todo?.status)}>
+							{todo?.status ? <MdDoneOutline /> : <MdPendingActions />}
+						</button>
+						<Link to="/">
+							<button onClick={() => handleDelete(id)}>
+								<AiFillDelete />
+							</button>
+						</Link>
+					</div>
+				</div>
+			) : null}
+		</div>
+	);
 }
+
 export default SingleTodoListing;
